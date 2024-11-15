082b64f7de50b31f011358c6696c3b54463e6ba2
shakibchakladar <shakibchakladar@gmail.com>
2024-11-16 02:40:32 +0600

    verify seller updated

--- index.js
+++ index.js
@@ -17,16 +17,16 @@ app.use(express.json());
 
 // token verification
 const verifyJWT=(req,res,next)=>{
-  const authorization=req.header.authorization;
+  const authorization=req.headers.authorization;
   if(!authorization){
     return res.send({message:'no token found'})
   }
-  const token=authorization.split( )[1];
+  const token=authorization.split(' ')[1];
   jwt.verify(token,process.env.ACCESS_KEY_TOKEN,(err,decoded)=>{
     if(err){
       return res.send({message: 'invalid token'})
     }
-    res.decoded=decoded;
+    req.decoded=decoded;
     next();
   })
 }
@@ -35,7 +35,7 @@ const verifyJWT=(req,res,next)=>{
 const verifySeller=async(req,res,next)=>{
   const email=req.decoded.email;
   const query={email:email}
-  const user=userCollection.findOne(query)
+  const user=await userCollection.findOne(query)
   if(user?.role !=='seller'){
     return res.send({message: 'Forbidden access'})
   }
